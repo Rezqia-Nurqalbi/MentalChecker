1f159a461d846554c4a854da9b9fc4b3dbe5f6d7
Rezqia-Nurqalbi <rezqianurqalbi8@gmail.com>
2023-05-30 14:40:32 +0800

    feat: melengkapi method pada kelas UserCheck

--- app/src/main/java/project/mental/Models/UserCheck.java
+++ app/src/main/java/project/mental/Models/UserCheck.java
@@ -17,25 +17,35 @@ public class UserCheck extends User {
     }
 
     void answerA(){
-        //kurang point 
+        mentalScore -= 0;
     }
 
     void answerB(){
-
+        mentalScore -= 5;
     }
 
     void answerC(){
-
+        mentalScore -= 10;
     }
 
     void showResult(){
         //pake pengkondisian 
+        if (mentalScore >= 0 && mentalScore <= 45) {
+            System.out.println("Sangat Buruk");
+        } else if (mentalScore >= 46 && mentalScore <= 70){
+            System.out.println("Buruk");
+        } else if (mentalScore >= 71 && mentalScore <= 85){
+            System.out.println("Cukup Baik");
+        } else if (mentalScore >= 86 && mentalScore <= 100){
+            System.out.println("Sangat Baik");
+        } else {
+            System.out.println("Score Mental Tidak valid");
+        }
+
     }
 
     @Override
     void greet(String Username) {
-    
-    }
-
-    
+        System.out.println("Selamat Datang Di Aplikasi ini" + Username + "...");
+    }   
 }
