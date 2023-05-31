38e6f1cad85fc4a65e8bbe49dec9c869f18a70a6
Rezqia-Nurqalbi <rezqianurqalbi8@gmail.com>
2023-05-31 16:03:48 +0800

    feat: menambahkan method pada kelas app

--- app/src/main/java/project/mental/App.java
+++ app/src/main/java/project/mental/App.java
@@ -5,65 +5,88 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class App extends Application {
-
-    private int currentQuestionIndex = 0;
-    private String[] pertanyaan = {
-        "Seperti apa perasaanmu saat ini?",
-        "Apa kamu merasa percaya diri dengan semua kondisimu sekarang?",
-        "Apakah Kamu Mudah Tersinggung dengan Omongan dan Respon Orang Lain Terhadapmu?",
-        "Apakah perasaan cemas atau tidak nyaman di sekitar orang lain mengganggu Anda?",
-        "Apakah Anda pernah merasa bahwa Anda telah dipengaruhi oleh perasaan gelisah, cemas, atau gugup?",
-        "Selama 12 bulan terakhir, seberapa sering Anda merasa sendirian atau kesepian?",
-        "Selama 12 bulan terakhir, seberapa sering Anda secara serius mempertimbangkan untuk mencoba bunuh diri?",
-        "Selama 12 bulan terakhir, apakah Anda pernah merencanakan bagaimana Anda akan mencoba bunuh diri?",
-        "Apakah Anda merasa tidak berharga atau merasa bersalah secara berlebihan?",
-        "Apakah Anda sering merasa kelelahan fisik dan mental tanpa alasan yang jelas?"
-    };
+    private Stage primaryStage;
+    private String name;
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Pertanyaan Mental");
-        Label pertanyaanLabel = new Label(pertanyaan[currentQuestionIndex]);
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        showStartScene();
+    }
 
-        Button jawabanAButton = new Button("A. Ya");
-        Button jawabanBButton = new Button("B. Tidak");
-        Button jawabanCButton = new Button("C. Kadang-kadang");
+    private void showStartScene() {
+        primaryStage.setTitle("Mental Checker");
 
-    
-        jawabanAButton.setOnAction(e -> {
-            nextQuestion(primaryStage);
-        });
-
-        jawabanBButton.setOnAction(e -> {
-            nextQuestion(primaryStage);
-        });
-
-        jawabanCButton.setOnAction(e -> {
-            nextQuestion(primaryStage);
-        });
+        Button startButton = new Button("Mulai");
+        startButton.setOnAction(e -> showNameInputScene());
 
         VBox layout = new VBox(10);
         layout.setAlignment(Pos.CENTER);
-        layout.getChildren().addAll(pertanyaanLabel, jawabanAButton, jawabanBButton, jawabanCButton);
+        layout.getChildren().add(startButton);
 
         Scene scene = new Scene(layout, 400, 300);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    private void nextQuestion(Stage stage) {
-        if (currentQuestionIndex < pertanyaan.length - 1) {
-            currentQuestionIndex++; // Pindah ke pertanyaan berikutnya
+    private void showNameInputScene() {
+        Label nameLabel = new Label("Masukkan nama Anda:");
+        TextField nameField = new TextField();
+        Button nextButton = new Button("Selanjutnya");
+        nextButton.setOnAction(e -> {
+            name = nameField.getText();
+            showMenuScene();
+        });
 
-            // Mengupdate label pertanyaan dengan pertanyaan berikutnya
-            ((Label) stage.getScene().getRoot().getChildrenUnmodifiable().get(0)).setText(pertanyaan[currentQuestionIndex]);
-        } else {
-            stage.close();
-        }
+        VBox layout = new VBox(10);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(nameLabel, nameField, nextButton);
+
+        Scene scene = new Scene(layout, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void showMenuScene() {
+        Label menuLabel = new Label("Pilih menu:");
+        Button mentalCheckerButton = new Button("Mental Checker");
+        mentalCheckerButton.setOnAction(e -> showMentalCheckerScene());
+        Button tekaTekiButton = new Button("Teka-Teki");
+        tekaTekiButton.setOnAction(e -> showTekaTekiScene());
+
+        VBox layout = new VBox(10);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(menuLabel, mentalCheckerButton, tekaTekiButton);
+
+        Scene scene = new Scene(layout, 400, 300);
+        primaryStage.setScene(scene);
+    }
+
+    private void showMentalCheckerScene() {
+        // Kode Anda untuk tampilan pertanyaan dan pilihan jawaban Mental Checker
+    }
+
+    private void showTekaTekiScene() {
+        // Kode Anda untuk tampilan teka-teki
+    }
+
+    private void showResultScene(String result) {
+        Label resultLabel = new Label("Hasil: " + result);
+        Button closeButton = new Button("Tutup");
+        closeButton.setOnAction(e -> primaryStage.close());
+
+        VBox layout = new VBox(10);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(resultLabel, closeButton);
+
+        Scene scene = new Scene(layout, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
